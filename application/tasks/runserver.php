0d81e39258d49083d2df1a2401e71ad567dd0225
adrian7722 <abottner199@gmail.com>
2013-05-09 15:01:31 +0200

    kolejny commit

--- application/tasks/runserver.php
+++ application/tasks/runserver.php
@@ -6,16 +6,16 @@ public function run($arg){
 
   if( !isset($arg[0])){
     $host="localhost";
-    $port="8080";
+    $port="8569";
  }else if(is_int($arg[0])){
     $host="localhost";
     $port=$arg[0];
  }else if(preg_match('@[a-z0-9.-]+\:[0-9]+@',$arg[0])){
     list($host,$port)=explode(':',$arg[0]);
  }else{
-    echo 'using default localhost:8080';
+    echo 'using default localhost:8569';
     $host="localhost"; 
-    $port=8080; 
+    $port=8569; 
  }
 
 echo 'Running PHP development server on port '.$port.'...';
---
+++ application/tests/jj.test.php
@@ -0,0 +1,15 @@
+<?php
+
+class Testjj extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test that a given condition is met.
+	 *
+	 * @return void
+	 */
+	public function testkk()
+	{
+		$this->assertTrue(true);	
+	}
+
+}
